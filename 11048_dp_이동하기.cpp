87000a53de5d65f10d144189568e89453a7ad36d
small-j <44703262+small-j@users.noreply.github.com>
2020-08-26 16:17:59 +0900

    Create 11048_dp_이동하기.cpp

---
+++ 11048_dp_이동하기.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int dist[1005][1005];
+int main()
+{
+	int N, M;
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++)
+		for (int j = 1; j <= M; j++)
+			cin >> dist[i][j];
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			if (dist[i - 1][j] < dist[i][j - 1]) dist[i][j] += dist[i][j - 1];
+			else dist[i][j] += dist[i - 1][j];
+		}
+	}
+	cout << dist[N][M] << endl;
+}
