e97ff63252396792dc4f4a90bb6e8acdc1f5d470
small-j <44703262+small-j@users.noreply.github.com>
2020-08-26 16:16:50 +0900

    Create 2410_dp_2의 멱수의 합.cpp

---
+++ 2410_dp_2의 멱수의 합.cpp
@@ -0,0 +1,21 @@
+```c++
+#include<iostream>
+using namespace std;
+int dist[1000005];
+int main()
+{
+	int N;
+	cin >> N;
+	
+	for (int i = 0; i <= N; i++)
+		dist[i] = 1;
+	for (int i = 2; i <= N; i *= 2) {
+		for (int j = 0; j <= N; j++) {
+			if (j < i) dist[j] = dist[j];
+			else
+				dist[j] = (dist[j - i] + dist[j]) % 1000000000;
+		}
+	}
+	cout << dist[N] << endl;
+}
+```
