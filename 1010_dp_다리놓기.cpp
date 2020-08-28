cd58123b14e59e2706188435259a1fecd86869d3
small-j <44703262+small-j@users.noreply.github.com>
2020-08-28 20:15:09 +0900

    Create 1010_dp_다리놓기.cpp

---
+++ 1010_dp_다리놓기.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int dist[35][35];
+int main()
+{
+	for (int i = 1; i < 31; i++)
+		dist[1][i] = i;
+
+	for (int i = 2; i < 31; i++) {
+		for (int j = i; j < 31; j++) {
+			for (int k = 1; k < j; k++) {
+				dist[i][j] += dist[i - 1][k];
+			}
+		}
+	}
+	int T;
+	cin >> T;
+	while (T--) {
+		int N, M;
+		cin >> N >> M;
+		cout << dist[N][M] << endl;
+	}
+}
