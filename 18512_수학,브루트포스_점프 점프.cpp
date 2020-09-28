545d46a3189911583ce0a69d2a03e8c49c1088f3
small-j <44703262+small-j@users.noreply.github.com>
2020-09-28 22:54:29 +0900

    Create 18512_수학,브루트포스_점프 점프.cpp

---
+++ 18512_수학,브루트포스_점프 점프.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int X, Y, P1, P2;
+	cin >> X >> Y >> P1 >> P2;
+	if (P1 < P2) {
+		int num = P1;
+		P1 = P2;
+		P2 = num;
+		num = X;
+		X = Y;
+		Y = num;
+	}
+
+	if (X == Y) {
+		if ((P1 - P2) % X == 0) cout << P1 << "\n";
+		else cout << -1 << "\n";
+		return 0;
+	}
+
+	int cnt = 0;
+	while (1) {
+		if ((P1 - P2) % Y == 0) {
+			cout << P1 << "\n";
+			break;
+		}
+		P1 += X;
+		cnt++;
+		if (cnt > 100) {
+			cout << -1 << "\n";
+			break;
+		}
+	}
+	return 0;
+}
