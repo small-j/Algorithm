b6be6b1a311f88adae6a8ca4259ab600ce8a727c
small-j <44703262+small-j@users.noreply.github.com>
2020-12-31 17:01:23 +0900

    Create 2502_떡먹는 호랑이.cpp

---
+++ 2502_떡먹는 호랑이.cpp
@@ -0,0 +1,32 @@
+
+//풀이
+//수학, dynamic programming
+
+#include<iostream>
+using namespace std;
+typedef long long ll;
+ll pow(ll num)
+{
+	return num * num;
+}
+ll spare(ll num)
+{
+	if (num == 1) return 2;
+	if (num == 0) return 1;
+	ll n = (pow(spare(num / 2))) % 1000000007;
+	if (num % 2 != 0) n = n * 2 % 1000000007;
+	return n;
+}
+int main()
+{
+	int T;
+	scanf("%d", &T);
+	while (T--)
+	{
+		ll N;
+		scanf("%lld", &N);
+		if (N == 1) printf("%d\n", 1);
+		else if (N == 2) printf("%d\n", 1);
+		else printf("%lld\n", spare(N - 2));
+	}
+}
