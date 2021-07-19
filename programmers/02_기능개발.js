cb2a020b1ff54fa68a02e39e80568fe518aa491b
small-j <44703262+small-j@users.noreply.github.com>
2021-07-19 15:36:09 +0900

    Create 02_기능개발.js

---
+++ programmers/02_기능개발.js
@@ -0,0 +1,22 @@
+
+//풀이
+//그리디
+
+function solution(progresses, speeds) {
+    var answer = [];
+    let Max = 0, cnt = 1;
+    for(let i = 0; i<progresses.length; i++) {
+        let timeItTakes = Math.floor((100 - progresses[i]) / speeds[i]);
+        if((100 - progresses[i]) % speeds[i] !== 0) timeItTakes += 1;
+        if(Max >= timeItTakes) cnt++;
+        else {
+            Max = timeItTakes;
+            if(i !== 0) {
+                answer.push(cnt);
+                cnt = 1;
+            }
+        }
+    }
+    answer.push(cnt);
+    return answer;
+}
